2b52ebf8921f80dc24814db72c02d2ec90e435a4
Andreas Schmidt <schmidt@nt.uni-saarland.de>
2014-12-19 18:36:56 +0100

    Add jsonPrettyFilter.

---
+++ aspects/filters/jsonPrettyFilter.js
@@ -0,0 +1,7 @@
+﻿(function(sdnViz, angular) {
+    sdnViz.filter("jsonPretty", function() {
+        return function(input) {
+            return angular.toJson(input, true);
+        };
+    });
+})(window.sdnViz, window.angular);
