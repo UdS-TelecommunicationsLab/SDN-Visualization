7f09fe690dc8ba570248273746a7aaf7e72fad9a
Andreas Schmidt <schmidt@nt.uni-saarland.de>
2015-03-19 14:40:12 +0100

    Add support for Math formulas.

---
+++ aspects/controls/mathjaxBind.js
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2013 - 2015 Saarland University
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * Contributor(s): Andreas Schmidt (Saarland University), Michael Karl (Saarland University)
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * This license applies to all parts of the SDN-Visualization Application that are not externally
+ * maintained libraries. The licenses of externally maintained libraries can be found in /node_modules and /lib.
+ */
+
+(function (sdnViz) {
+    "use strict";
+
+    // Source: http://stackoverflow.com/questions/16087146/getting-mathjax-to-update-after-changes-to-angularjs-model
+
+    sdnViz.directive("mathjaxBind", function () {
+        return {
+            restrict: "A",
+            controller: ["$scope", "$element", "$attrs",
+                function ($scope, $element, $attrs) {
+                    $scope.$watch($attrs.mathjaxBind, function (texExpression) {
+                        var texScript = angular.element("<script type='math/tex'>")
+                            .html(texExpression ? texExpression : "");
+                        $element.html("");
+                        $element.append(texScript);
+                        MathJax.Hub.Queue(["Reprocess", MathJax.Hub, $element[0]]);
+                    });
+                }]
+        };
+    });
+})(window.sdnViz);
