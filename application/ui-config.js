822cfcb9abc66820f2f1fb0a79cdaeb25892a79a
Andreas Schmidt <schmidt@nt.uni-saarland.de>
2015-05-26 13:47:14 +0200

    Small fixes decrease number of file accesses.

--- application/ui-config.js
+++ application/ui-config.js
@@ -28,10 +28,11 @@
 (function () {
     "use strict";
     var fs = require("fs"),
+        config = require("./config"),
         mime = require("mime");
 
     var sdnc = __dirname +  "/../sdn-conf.json";
-    var config = null;
+    var uiConfigurationObject = null;
     var callbacks = [];
     var sdnUiConfig = __dirname + "/../sdn-ui-conf.json";
     var sdnUiSampleConfig = __dirname + "/../sdn-ui-conf.default.json";
@@ -44,9 +45,9 @@
     };
 
     var readConfig = function () {
-        config = JSON.parse(fs.readFileSync(sdnUiConfig, {encoding: "utf-8"}));
+        uiConfigurationObject = JSON.parse(fs.readFileSync(sdnUiConfig, {encoding: "utf-8"}));
         callbacks.forEach(function (cb) {
-            cb(config);
+            cb(uiConfigurationObject);
         });
     };
 
@@ -82,25 +83,25 @@
 
     exports.getConfiguration = function () {
         createFileIfNotExistent();
-        if (config === null) {
+        if (uiConfigurationObject === null) {
             readConfig();
         }
 
-        return config;
+        return uiConfigurationObject;
     };
 
     exports.saveConfiguration = function (configData) {
         // read ctrl config
-        var sdncJson = JSON.parse(fs.readFileSync(sdnc, {encoding: "utf-8"}));
+        var sdncJson = config.getConfiguration();
         if(sdncJson.isDemoMode){
             // in demo mode (.isDemoMode == true) rewrite old config
-            var configOld = JSON.parse(fs.readFileSync(sdnUiConfig, {encoding: "utf-8"}));
+            var configOld = exports.getConfiguration();
             configData.dataSource.type = configOld.dataSource.type;
             configData.dataSource.connectionString = configOld.dataSource.connectionString;
             fs.writeFileSync(sdnUiConfig, JSON.stringify(configData));
-            return; // and get out
+        } else {
+            fs.writeFileSync(sdnUiConfig, JSON.stringify(configData));
         }
-        fs.writeFileSync(sdnUiConfig, JSON.stringify(configData));
     };
 
     exports.registerHandler = function (cb) {
